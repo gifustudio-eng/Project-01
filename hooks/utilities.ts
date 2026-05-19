0b1667d35ace20fa7d9149b7c3349f006553a780
Tekn-drive <juanwishnutopia@gmail.com>
2026-05-19 13:14:10 +0700

    Rearranged Tweets based on Post Date and Time

--- hooks/utilities.ts
+++ hooks/utilities.ts
@@ -20,7 +20,8 @@ export function useFetchTweets() {
           .select("id, tweet, replies (id, reply_text)")
           .eq("relevant", true)
           .neq("priority", "low")
-          .eq("user_id", user?.id);
+          .eq("user_id", user?.id)
+          .order("created_at", { ascending: false });
     
         if (!error) {
           setTweets(data || []);
