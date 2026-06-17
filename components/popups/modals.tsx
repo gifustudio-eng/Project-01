b88c12c8c0032bc2aed0eeefc6b58d40b98d4a8c
Tekn-drive <juanwishnutopia@gmail.com>
2026-06-17 23:04:52 +0700

    Updated Approved Reply Popup to include Posted

--- components/popups/modals.tsx
+++ components/popups/modals.tsx
@@ -6,7 +6,7 @@ export function AcceptedModal({ onClose }: { onClose: () => void }) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
       <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg text-center dark:text-zinc-50">
-        <h1>Reply Approved!</h1>
+        <h1>Reply Posted and Approved!</h1>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
